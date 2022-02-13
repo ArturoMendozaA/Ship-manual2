557d3dc7650f8d6df156de26480ecf695635249a
ArturoMendozaA <99451129+ArturoMendozaA@users.noreply.github.com>
2022-02-13 04:03:53 -0600

    Añade calc.py

---
+++ Calc.py
@@ -0,0 +1,4 @@
+print('Calculadora')
+primer_numero = input('Ingresa el primer numero: ')
+segundo_numero = input('Ingresa el segundo numero: ')
+print('El resultado es: ' +  str(int(primer_numero) + int(segundo_numero)))
